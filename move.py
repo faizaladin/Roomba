e5670ab44faf2790c2e3da10c65f29e312d92e82
faizaladin <faizaladin365@gmail.com>
2024-04-30 13:33:35 -0400

    changed threshold

--- move.py
+++ move.py
@@ -88,7 +88,7 @@ def move():
         for scan in lidar.iter_scans(max_buf_meas=5000):
             obstacle_detected = False
             for (_, angle, distance) in scan:
-                if distance < 520 and (angle < 15 or angle > 345):
+                if distance < 300 and (angle < 15 or angle > 345):
                     obstacle_detected = True
                     break
                 print("Angle: {}, Distance: {}".format(angle, distance))
