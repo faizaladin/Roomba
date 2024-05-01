f2087a462422d4641d886495410a748f7146ec6b
faizaladin <faizaladin365@gmail.com>
2024-05-01 00:47:36 -0400

    error fix

--- move.py
+++ move.py
@@ -95,14 +95,16 @@ def spiral():
         left(0.5)
         for scan in lidar.iter_scans(max_buf_meas=5000):
             for (_, angle, distance) in scan:
+                print("Angle: {}, Distance: {}".format(angle, distance))
                 if distance < 550 and (angle < 15 or angle > 345):
                     print("object detected")
                     return move()
                 else:
                     print("in else statement")
+                    set_forward(counter)
+                    left(0.5)
                     counter += 0.5
                     break
-                print("Angle: {}, Distance: {}".format(angle, distance))
 
 
 def move():
